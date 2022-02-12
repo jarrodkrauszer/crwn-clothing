6610af25945c305fa3edd95df71d0447be6b8e7a
jkrauszer <jarrod@Jarrods-MacBook-Pro.local>
2022-02-12 09:24:07 -0500

    updating strip integration and updating our favicon and title

--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -1,5 +1,7 @@
 import { createSelector } from 'reselect';
 
+import memoize from 'lodash.memoize';
+
 const selectShop = state => state.shop;
 
 export const selectCollections = createSelector(
@@ -12,8 +14,9 @@ export const selectCollectionsForPreview = createSelector(
   collections => Object.keys(collections).map(key => collections[key])
 );
 
-export const selectCollection = collectionUrlParam =>
+export const selectCollection = memoize(collectionUrlParam =>
   createSelector(
     [selectCollections],
     collections => collections[collectionUrlParam]
-  );
+  )
+);
